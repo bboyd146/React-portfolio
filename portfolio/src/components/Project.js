197d5a08bb1b4f977d7bcd94ade329d79009eba9
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-16 22:12:35 -0500

    const change from Example to Project

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -36,7 +36,7 @@ const projects = [
     // More Projects...
 ]
 
-export default function Example() {
+export default function Project() {
     return (
         <div className="bg-white">
             <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
