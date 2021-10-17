61d1b995430265c001c8220f703ec2ce69beb0a2
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-16 20:08:12 -0500

    footer rendered

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from './components/Navbar';
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <Home />
+      <Footer />
     </main>
   );
 }
