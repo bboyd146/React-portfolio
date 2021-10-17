bdc33c109ecaa6924320d7fb4fa44442ade1af7c
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-17 00:01:41 -0500

    projects component complete

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -5,13 +5,15 @@ import { projects } from '../projects';
 
 export default function Project() {
     return (
-        <div className="bg-white">
+        <>
+        <h1 className=" border-t-2 border-gray-300 text-4xl tracking-tight bg-white font-extrabold flex-1 flex items-center justify-center pt-10 text-pink-700 sm:text-5xl md:text-6xl">Work</h1>
+        <div className="bg-white ">
             <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                 <h2 className="sr-only">Projects</h2>
 
                 <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                     {projects.map((project) => (
-                        <a key={project.id} href={project.href} className="group">
+                        <a key={project.id} href={project.dHref} className="group">
                             <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                                 <img
                                     src={project.imageSrc}
@@ -21,10 +23,12 @@ export default function Project() {
                             </div>
                             <h3 className="mt-4 text-sm text-gray-700">{project.name}</h3>
                             <p className="mt-1 text-lg font-medium text-gray-900">{project.price}</p>
+                            <button href={project.gHref}>Git Link</button>
                         </a>
                     ))}
                 </div>
             </div>
         </div>
+        </>
     )
 }
