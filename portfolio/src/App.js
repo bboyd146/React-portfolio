d145b109b9220689fcbb5094b4481db2843ea824
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-16 22:14:45 -0500

    installed aspect/radio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from './components/Navbar';
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-
+import Project from "./components/Project";
 
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <Home />
+      <Project />
       <Footer />
     </main>
   );
