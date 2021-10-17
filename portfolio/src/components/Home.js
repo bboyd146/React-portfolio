c40a341fb05feedc6bc58b7fc2e1a5d67454abb3
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-16 20:28:43 -0500

    updated: changed Portfolio to Resume

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -104,7 +104,7 @@ export default function Home() {
                                         href="#"
                                         className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-pink-900 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                                     >
-                                        Portfolio
+                                        Resume
                                     </a>
                                 </div>
                             </div>
