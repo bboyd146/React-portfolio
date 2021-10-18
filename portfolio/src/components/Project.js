0d0d7b476569e0465860876601c7bc7a04402eca
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-18 00:52:45 -0500

    git hub links aligned with cards

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -13,18 +13,23 @@ export default function Project() {
 
                     <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                         {projects.map((project) => (
-                            <a key={project.id} href={project.dHref} className="group">
-                                <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                                    <img
-                                        src={project.imageSrc}
-                                        alt={project.imageAlt}
-                                        className="w-full h-full object-center object-cover group-hover:opacity-75"
-                                    />
+                            <>
+                                <a key={project.id} href={project.dHref} className="group">
+                                    <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                                        <img
+                                            src={project.imageSrc}
+                                            alt={project.imageAlt}
+                                            className="w-full h-full object-center object-cover group-hover:opacity-75"
+                                        />
+                                    </div>
+                                    <h3 className="mt-4 text-sm text-gray-700">{project.name}</h3>
+                                    <p className="mt-1 text-lg font-medium text-gray-900">{project.price}</p>
+                                <div className="pt-10 sm:mt-0 sm:ml-3">
+                                    <a href={project.gHref} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-xs font-medium rounded-md text-white bg-pink-900 hover:bg-indigo-200 md:py-4 md:text-xs md:px-10">
+                                        <button>Git Link</button></a>
                                 </div>
-                                <h3 className="mt-4 text-sm text-gray-700">{project.name}</h3>
-                                <p className="mt-1 text-lg font-medium text-gray-900">{project.price}</p>
-                                <button href={project.gHref}>Git Link</button>
-                            </a>
+                                </a>
+                            </>
                         ))}
                     </div>
                 </div>
