b9b81faa8084ac7bfe61c8798062b7d046b22a10
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-17 20:28:10 -0500

    about me component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,13 +4,14 @@ import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
+import Container from "./components/Container";
 
 function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <Home />
-      <Project />
+      <Container />
       <Footer />
     </main>
   );
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,16 @@
+
+export default function About() {
+    return (
+        <>
+            <h1 className=" border-t-2 border-gray-300 text-4xl tracking-tight bg-white font-extrabold flex-1 flex items-center justify-center pt-10 text-pink-700 sm:text-5xl md:text-6xl">About Me</h1>
+            <div className="bg-white ">
+                <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <h2 className="sr-only">Me</h2>
+                    <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                        An up and coming Software Developer who’s currently attending Rice University’s Online Coding Bootcamp. Apart from my driving experience from previous employment, I'm learning new skills to become a polished Full Stack Developer. I have the skills necessary to build a full stack application, which includes Front & Back End development.
+                        I'm looking forward to build more projects to hone my skills and become a better developer as time goes on!</p>
+                </div>
+            </div>
+        </>
+    )
+}
