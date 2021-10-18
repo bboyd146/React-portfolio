e851cc6e81003c448d9bd336977b5a198c257dcb
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-17 23:37:54 -0500

    centered text

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
             <div className="bg-white">
                 <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                     <h2 className="sr-only">Me</h2>
-                    <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                    <p className="mt-3 text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 ">
                         An up and coming Software Developer who’s currently attending Rice University’s Online Coding Bootcamp. Apart from my driving experience from previous employment, I'm learning new skills to become a polished Full Stack Developer. I have the skills necessary to build a full stack application, which includes Front & Back End development.
                         I'm looking forward to build more projects to hone my skills and become a better developer as time goes on!</p>
                 </div>
