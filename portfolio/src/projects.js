6d270b6b57c3fc2d63f2399a791a2c3981bf245c
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-02-06 15:05:48 -0600

    updated project properties and order

--- portfolio/src/projects.js
+++ portfolio/src/projects.js
@@ -1,57 +1,63 @@
 export const projects = [
     {
         id: 1,
-        name: 'Progressive Budget',
-        gHref: 'https://github.com/bboyd146/on-offline-budget-tracker.git',
-        dHref: 'https://on-offline-budget-tracker.onrender.com',
-        price: 'This pwa app is designed to allow a user to budget their finances by adding or deducting funds on/offline.',
-        imageSrc: './progress.png',
-        imageAlt: 'pwa app screenshot',
-    },
-    {
-        id: 2,
-        name: 'Workout Tracker',
-        gHref: 'https://github.com/bboyd146/workout-tracker.git',
-        dHref: 'https://workout-tracker-p8aj.onrender.com',
-        price: 'This Full-stack project is designed to allow a user to post and keep track of previous workouts.',
-        imageSrc: './workout.png',
-        imageAlt: 'workout tracker screenshot',
-    },
-    {
-        id: 3,
         name: 'Cosmic Records',
         gHref: 'https://github.com/bboyd146/cosmic-repo.git',
         dHref: 'https://cosmic-repo.onrender.com',
         price: 'This e-commerce project is designed to allow music lovers and collectors to search and purchase vinyls to add to their collection. More of a proof of concept - stripe technology incorporated to process payments.',
         imageSrc: './cosmic.png',
         imageAlt: 'Cosmic Records app screenshot',
+        tech: ['React', 'Tailwind', 'JWT', 'Express', 'Node', 'Apollo', 'MongoDB', 'ApolloGraphql', 'Redux', 'Stripe']
+    },
+    {
+        id: 2,
+        name: 'Progressive Budget',
+        gHref: 'https://github.com/bboyd146/on-offline-budget-tracker.git',
+        dHref: 'https://on-offline-budget-tracker.onrender.com',
+        price: 'This pwa app is designed to allow a user to budget their finances by adding or deducting funds on/offline.',
+        imageSrc: './progress.png',
+        imageAlt: 'pwa app screenshot',
+        tech: ['Javascript', 'CSS', 'Express', 'Webpack', 'Html5', 'MongoDB', 'Mongoose', 'Node']
+    },
+    {
+        id: 3,
+        name: 'Workout Tracker',
+        gHref: 'https://github.com/bboyd146/workout-tracker.git',
+        dHref: 'https://workout-tracker-p8aj.onrender.com',
+        price: 'This Full-stack project is designed to allow a user to post and keep track of previous workouts.',
+        imageSrc: './workout.png',
+        imageAlt: 'workout tracker screenshot',
+        tech: ['Javascript', 'CSS', 'Express', 'Node', 'Mongoose', 'Chart.js']
     },
     {
         id: 4,
-        name: 'Work Day Scheduler',
-        gHref: 'https://github.com/bboyd146/work-day-scheduler.git',
-        dHref: 'https://bboyd146.github.io/work-day-scheduler/',
-        price: 'A simple calendar application that allows a user to save events for each hour of the day',
-        imageSrc: './scheduler.png',
-        imageAlt: 'Work day scheduler screenshot',
-    },
-    {
-        id: 5,
-        name: 'Password Generator',
-        gHref: 'https://github.com/bboyd146/password-generator.git',
-        dHref: 'https://bboyd146.github.io/password-generator/',
-        price: 'An application designed to generate a random password based off of user input',
-        imageSrc: './password.png',
-        imageAlt: 'Password generator screenshot',
-    },
-    {
-        id: 6,
         name: 'Employee Tracker',
         gHref: 'https://drive.google.com/file/d/1o5MKdk2P0zLRiqoCtMnfMocd8b1BfnD3/view',
         dHref: 'https://drive.google.com/file/d/1o5MKdk2P0zLRiqoCtMnfMocd8b1BfnD3/view',
         price: 'This project is a CLI content management system for managing a company\'s employees using node, inquirer, and MySQL',
         imageSrc: './employee.gif',
         imageAlt: 'Gif for employee tracker',
+        tech: ['Javascript', 'MySQL', 'Express', 'Node', 'Inquirer']
+    },
+    {
+        id: 5,
+        name: 'Work Day Scheduler',
+        gHref: 'https://github.com/bboyd146/work-day-scheduler.git',
+        dHref: 'https://bboyd146.github.io/work-day-scheduler/',
+        price: 'A simple calendar application that allows a user to save events for each hour of the day',
+        imageSrc: './scheduler.png',
+        imageAlt: 'Work day scheduler screenshot',
+        tech: ['Javascript', 'CSS', 'Html5']
+    },
+    {
+        id: 6,
+        name: 'Password Generator',
+        gHref: 'https://github.com/bboyd146/password-generator.git',
+        dHref: 'https://bboyd146.github.io/password-generator/',
+        price: 'An application designed to generate a random password based off of user input',
+        imageSrc: './password.png',
+        imageAlt: 'Password generator screenshot',
+        tech: ['Javascript', 'CSS', 'Html5']
     },
 ]
     // More Projects..
